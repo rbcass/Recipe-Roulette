aec8f811cb10f0a9fda8b4dcf1983a7db7ac78a4
rbcass <sodiumhydroxide666@gmail.com>
2023-11-12 23:24:35 +0000

    login & routing

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const path = require('path')
 const router = require('./server/routes/routes')
-const port= 3000;
+const port= 4000;
 
 //imports
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 //view engine
-app.set('layout', './layouts/main')
+// app.set('layout', './layouts/main')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(expressLayouts)
 app.use('/',router)
 
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,7 +6,11 @@ const router = express.Router()
 
 
 router.get('/', (req,res) =>{
-    res.render('index')
+    res.render('main')
+})
+
+router.get('/login', (req,res) =>{
+    res.render('login')
 })
 
 module.exports = router;
